f120659d91146ecdf25ccec0da2ba3ea50b08e78
Quin Mundell <6mundq94@solent.ac.uk>
2022-10-06 11:23:32 +0100

    If statement added

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-print("Hello, world")
+# print("Hello, world")
+#
+# name = input("Please enter a name: ")
+# print(f"My name is {name}")
+age = int(input("Please enter a age: "))
 
-name = input("Please enter a name: ")
-print(f"My name is {name}")
+if age <= 5:
+    print("Free")
+
+elif 18 > age > 5:
+    print("£300")
+
+else:
+    print("1")
+
